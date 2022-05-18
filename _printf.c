9ea5828df96fad8c2dba1f4d5182a26ba833d180
Omosquary <iamomosquare@gmail.com>
2022-05-18 02:41:58 -0700

    Update Task 0

--- _printf.c
+++ _printf.c
@@ -1,78 +1,36 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
-/**
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, octal, hexadecimal, string, int, decimal)
-  * Return: NULL or function associated.
-  */
-int (*find_function(const char *format))(va_list)
-{
-	unsigned int i = 0;
-	code_f find_f[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
-		{NULL, NULL}
-	};
+#include <stdio.h>
 
-	while (find_f[i].sc)
+/**
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
+ */
+
+int _printf(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
 	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else
+		{
+			/* format[i] == '%' */
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				i++;
+			}
+		}
 		i++;
 	}
-	return (NULL);
-}
-/**
-  * _printf - function that produces output according to a format.
-  * @format: format (char, string, int, decimal)
-  * Return: size the output text;
-  */
-int _printf(const char *format, ...)
-{
-	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, cprint = 0;
+	va_end(args);
 
-	if (format == NULL)
-		return (-1);
-	va_start(ap, format);
-	while (format[i])
-	{
-		while (format[i] != '%' && format[i])
-		{
-			_putchar(format[i]);
-			cprint++;
-			i++;
-		}
-		if (format[i] == '\0')
-			return (cprint);
-		f = find_function(&format[i + 1]);
-		if (f != NULL)
-		{
-			cprint += f(ap);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		cprint++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
-	}
-	va_end(ap);
-	return (cprint);
+	return (0);
 }
---
+++ test/main2.c
@@ -0,0 +1,9 @@
+#include "../main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	_printf("Hello %c Rofiat\n", 'A');
+	printf("Hello %c Rofiat\n", 'A');
+	return (0);
+}
