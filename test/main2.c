3e03513a1c7aab02cde01d7dd3402512ca9c80ce
Omosquary <iamomosquare@gmail.com>
2022-05-18 03:13:30 -0700

    Taking Strings Perfectly

--- test/main2.c
+++ test/main2.c
@@ -5,5 +5,8 @@ int main(void)
 {
 	_printf("Hello %c Rofiat\n", 'A');
 	printf("Hello %c Rofiat\n", 'A');
+	_printf("Hello %s Rofiat\n", "World");
+	printf("Hello %s Rofiat\n", "World");
+
 	return (0);
 }
