8423ed105b060c49a47d179546124e4fdc84163c
Omosquary <iamomosquare@gmail.com>
2022-05-18 04:08:02 -0700

    pull_print

--- pull_print.c
+++ pull_print.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
-#include "mainn.h"
+#include "main.h"
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
@@ -10,3 +11,44 @@
  **/
 int pull_print(char c, va_list *ap)
 {
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			output++;
+		}
+		else
+		{
+			/* format[i] == '%' */
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				output++;
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				i++;
+				ptr = va_arg(args, char *);
+				while (ptr[x] != '\0')
+				{
+					_putchar(ptr[x]);
+					output++;
+					x++;
+				}
+			}
+			else if (format[i + 1] == '%')
+			{
+				output++;
+				i++;
+				_putchar('%');
+			}
+		}
+		i++;
+	}
+	va_end(args);
+	return (output);
+}
