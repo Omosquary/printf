ba3fda6aa3e9d7c1400921b2fdafdf9f40c1cb8a
Omosquary <iamomosquare@gmail.com>
2022-05-19 02:02:01 -0700

    Still on Task 0

--- print_char.c
+++ print_char.c
@@ -9,9 +9,9 @@
 
 int _print_char(va_list args)
 {
-	char c = va_arg(args, int);
+	unsigned char c;
 
+	c = va_arg(args, int);
 	_putchar(c);
 	return (1);
-
 }
--- print_str.c
+++ print_str.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
 /**
  * _print_str - is a function used to display a string
  * @args: receives the list of arguments passed
- * Return: Value
+ * Return: 1
  */
 
 int _print_str(va_list args)
@@ -12,10 +14,13 @@ int _print_str(va_list args)
 	int x = 0;
 	char *s = va_arg(args, char *);
 
+	if (s == NULL)
+		s = "(null)";
+
 	while (s[x] != '\0')
 	{
 		_putchar(s[x]);
 		x++;
 	}
-	return (0);
+	return (x);
 }
