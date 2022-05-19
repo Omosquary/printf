9ec8806a07f33acbf0504de59282b7bf07ef5342
Omosquary <iamomosquare@gmail.com>
2022-05-19 03:04:26 -0700

    Task 0

--- main.h
+++ main.h
@@ -12,4 +12,16 @@ int _printf(const char *format, ...);
 int _print_perc(va_list args);
 int (*_select_func(char c))(va_list);
 
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list);
+} ph;
+
 #endif /* end MAIN_H */
